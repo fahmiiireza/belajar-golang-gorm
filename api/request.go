b55f0dce7fc2c4c09451514d9b37e532bbc531bf
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-04-24 15:33:03 +0200

    change json from snakecase to camelCase

--- api/request.go
+++ api/request.go
@@ -12,30 +12,32 @@ type LibrarianRequest struct {
 	Username         string              `json:"username" binding:"required"`
 	Email            string              `json:"email" binding:"required,email"`
 	Password         string              `json:"password" binding:"required"`
-	FullName         string              `json:"full_name" binding:"required"`
+	FullName         string              `json:"fullName" binding:"required"`
 	Salary           int                 `json:"salary" binding:"required"`
-	EmploymentStatus db.EmploymentStatus `json:"employment_status" binding:"required"`
-	JoiningDate      time.Time           `json:"joining_date" binding:"required"`
+	EmploymentStatus db.EmploymentStatus `json:"employmentStatus" binding:"required"`
+	JoiningDate      time.Time           `json:"joiningDate" binding:"required"`
 }
 
 type AdminRequest struct {
 	Username         string              `json:"username" binding:"required"`
 	Email            string              `json:"email" binding:"required,email"`
 	Password         string              `json:"password" binding:"required"`
-	FullName         string              `json:"full_name" binding:"required"`
+	FullName         string              `json:"fullName" binding:"required"`
 	Salary           int                 `json:"salary" binding:"required"`
-	EmploymentStatus db.EmploymentStatus `json:"employment_status" binding:"required"`
+	EmploymentStatus db.EmploymentStatus `json:"employmentStatus" binding:"required"`
 }
 
 type UserUpdate struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
-	FullName string `json:"full_name"`
+	FullName string `json:"fullName"`
 }
+
 type LibrarianUpdate struct {
 	Salary           int                 `json:"salary"`
-	EmploymentStatus db.EmploymentStatus `json:"employment_status"`
+	EmploymentStatus db.EmploymentStatus `json:"employmentStatus"`
 }
+
 type LibrarianUpdateRequest struct {
 	User      UserUpdate      `json:"user"`
 	Librarian LibrarianUpdate `json:"librarian"`
@@ -50,8 +52,8 @@ type BookRequest struct {
 	Title       string `json:"title" binding:"required"`
 	ISBN        string `json:"isbn" binding:"required"`
 	Language    string `json:"language" binding:"required"`
-	TotalCopy   int    `json:"total_copy" binding:"required"`
-	ShelfID     uint   `json:"shelf_id" binding:"required"`
-	CategoryID  uint   `json:"category_id" binding:"required"`
+	TotalCopy   int    `json:"totalCopy" binding:"required"`
+	ShelfID     uint   `json:"shelfId" binding:"required"`
+	CategoryID  uint   `json:"categoryId" binding:"required"`
 	Description string `json:"description"`
 }
