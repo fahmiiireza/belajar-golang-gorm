210b879f412ea86f325872e1fea027492a61cbbf
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-04-25 13:25:55 +0200

    added book-service with typescript on the project

---
+++ book-service/index.ts
@@ -0,0 +1,14 @@
+// index.ts
+
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Node.js server is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
